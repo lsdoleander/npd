37e78bcdd0752be176bc16b5fd3d15e76afbf415
LSD.oleander <lsdoleander@gmail.com>
2025-08-08 08:41:53 -0400

    edits

--- webapp/www/index.js
+++ webapp/www/index.js
@@ -1,28 +1,32 @@
+$(document).ready(function(){
+  const template = $("#table-template").detach().html();
 
-const template = $("#table-template").detach().html();
-
-$( "#search" ).on( "submit", async event => {
-  event.preventDefault();
-
-  let results = await fetch("/search", { data: $(this).serialize() });
-  if (results.length > 0) {
-    $("#welcome").hide();
-    let table = $(template);
-    let tbody = $(table).find("tbody");
-    for (let i of results) {
-      tbody.attach($(`<tr>
-        <td>${i.first}</td>
-        <td>${i.middle}</td>
-        <td>${i.last}</td>
-        <td>${i.address}</td>
-        <td>${i.city}</td>
-        <td>${i.state}</td>
-        <td>${i.zip}</td>
-        <td>${i.phone}</td>
-        <td>${i.dob}</td>
-        <td>${i.ssn}</td>
-      </tr>`));
-    }
-    $("#results").attach(table);
-  }
+  $("#search").on("submit", event => {
+    event.preventDefault();
+    return false;
+    
+    (async()=>{
+      let results = await fetch("/search", { data: $(this).serialize() });
+      if (results.length > 0) {
+        $("#welcome").hide();
+        let table = $(template);
+        let tbody = $(table).find("tbody");
+        for (let i of results) {
+          tbody.attach($(`<tr>
+            <td>${i.first}</td>
+            <td>${i.middle}</td>
+            <td>${i.last}</td>
+            <td>${i.address}</td>
+            <td>${i.city}</td>
+            <td>${i.state}</td>
+            <td>${i.zip}</td>
+            <td>${i.phone}</td>
+            <td>${i.dob}</td>
+            <td>${i.ssn}</td>
+          </tr>`));
+        }
+        $("#results").attach(table);
+      }
+    })
+  });
 });
