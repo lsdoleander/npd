cc9487714403a7aac0af3cd30f29f790cdbb865f
LSD.oleander <lsdoleander@gmail.com>
2025-08-08 08:24:41 -0400

    web

---
+++ webapp/express.js
@@ -0,0 +1,50 @@
+
+import express from 'express';
+import cors from 'cors';
+import postgres from 'postgres';
+
+(function start(){
+
+	const sql = postgres('postgres://postgres:postgres@db:5432/npd')
+
+	const LAYOUT = { "1":68, "2":40 };
+
+	let app = express();
+	app.use(cors());
+	app.use(express.static('www'))
+
+	app.post("/search", async (req,res)=>{
+		let results = [];
+
+		for (let spa in LAYOUT) {
+			for (const spb = 1; spb <= LAYOUT[spa]; spb++) {
+				try {
+					let where = false;
+
+					function param(name) {
+						if (req.params[name] && req.params[name].trim() !== "") {
+							const value = req.params[name];
+							const like = value.indexOf("%") > -1;
+							let out = sql`${where?"AND":"WHERE"} ${name} ${like ? "LIKE":"="} ${value}`
+							where = true
+							return out
+						}
+					}
+
+					const table = `npd_${spa}_${spb < 10 ? "0"+spb : spb}`;
+					const hits = await sql`select (first, middle, last, address, city, state, zip, phone, dob, ssn from ${table} 
+						${param('first')} ${param('last')} ${param('city')} ${param('state')} ${param('zip')}`
+					if (hits && hits.length > 0) {
+						results = [...results, ...hits];
+					}
+				} catch (ex) {
+					// 2 BAD
+				}
+			}
+		}
+
+		res.json(results);
+	})
+
+	app.listen(8989);
+})()
---
+++ webapp/www/index.js
@@ -0,0 +1,28 @@
+
+const template = $("#table-template").detach().html();
+
+$( "#search" ).on( "submit", async event => {
+  event.preventDefault();
+
+  let results = await fetch("/search", { data: $(this).serialize() });
+  if (results.length > 0) {
+    $("#welcome").hide();
+    let table = $(template);
+    let tbody = $(table).find("tbody");
+    for (let i of results) {
+      tbody.attach($(`<tr>
+        <td>${i.first}</td>
+        <td>${i.middle}</td>
+        <td>${i.last}</td>
+        <td>${i.address}</td>
+        <td>${i.city}</td>
+        <td>${i.state}</td>
+        <td>${i.zip}</td>
+        <td>${i.phone}</td>
+        <td>${i.dob}</td>
+        <td>${i.ssn}</td>
+      </tr>`));
+    }
+    $("#results").attach(table);
+  }
+});
