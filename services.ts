87a07c7f166b848f879a79c3c902eb55d99c3e0a
LSD.oleander <lsdoleander@gmail.com>
2025-09-18 05:38:02 -0400

    oneliner fix

--- services.ts
+++ services.ts
@@ -10,5 +10,5 @@ export const createTableIfNotExists = async (client: PoolClient, config: AppConf
 };
 
 export const createIndex = async (client: PoolClient, table: string, suffix: string): Promise<void> => {
-  await client.query(`CREATE INDEX index_name_by_location_${suffix} ON ${table} (first, last, city, state, zip) INCLUDE (middle, dob, address, county, phone, since, ssn)`)
+  await client.query(`CREATE INDEX IF NOT EXISTS index_name_by_location_${suffix} ON ${table} (first, last, city, state, zip) INCLUDE (middle, dob, address, phone, ssn)`)
 }
