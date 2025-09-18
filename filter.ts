38feb97dcf8a17e819779aa9fc79d11227d00050
LSD.oleander <lsdoleander@gmail.com>
2025-09-18 06:36:40 -0400

    oneliner fix

--- filter.ts
+++ filter.ts
@@ -1,6 +1,6 @@
 
-import * as stream from node:stream;
-import { createWriteStream, type WriteStream } from node:fs;
+import * as stream from 'node:stream';
+import { createWriteStream, type WriteStream } from 'node:fs';
 
 export class CSVCommaSpaceEscaper extends stream.Transform {
   remain:string = "";
