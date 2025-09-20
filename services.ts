6f4850f36caab75c2ead1dad40502471f3ad1a13
LSD.oleander <lsdoleander@gmail.com>
2025-09-20 09:50:29 -0400

    maint

--- services.ts
+++ services.ts
@@ -10,8 +10,8 @@ export const createTableIfNotExists = async (client: PoolClient, config: AppConf
 };
 
 export const createIndex = async (client: PoolClient, table: string, suffix: string): Promise<void> => {
-  await client.query(`DROP INDEX IF EXISTS index_name_by_location_${suffix}`);
   await client.query(`CREATE INDEX IF NOT EXISTS npd${suffix}_search_index ON ${table} (first, last, city, state, zip, ssn) INCLUDE (id, middle, suffix, dob, address, phone, altdob1)`)
+  console.log(`created npd${suffix}_search_index`)
 }
 
 export const refactorIndices = async(client: PoolClient): Promise<void> => {
@@ -19,6 +19,10 @@ export const refactorIndices = async(client: PoolClient): Promise<void> => {
     for (let spa in LAYOUT) {
       for (let spb = 1; spb <= LAYOUT[spa]; spb++) {
         const suffix = `_${spa}_${spb < 10 ? "0"+spb : spb}`;
+        await client.query(`DROP INDEX IF EXISTS index_name_by_location_${suffix}`);
+        console.log(`dropped index_name_by_location_${suffix}`)
+        await client.query(`ALTER TABLE npd${suffix} DROP COLUMN IF EXISTS aka1, DROP COLUMN IF EXISTS aka2, DROP COLUMN IF EXISTS aka3, DROP COLUMN IF EXISTS since, DROP COLUMN IF EXISTS altdob2, DROP COLUMN IF EXISTS altdob3`);
+        console.log(`altered npd${suffix}`)
         await createIndex(client, "npd"+suffix, suffix);
       }
     }
