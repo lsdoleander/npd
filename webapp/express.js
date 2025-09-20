bfd6fcde58324052ea8b7fea1e1a843fcc153997
LSD.oleander <lsdoleander@gmail.com>
2025-09-20 11:57:48 -0400

    maint

--- webapp/express.js
+++ webapp/express.js
@@ -1,7 +1,9 @@
-
+import ws from 'express-ws';
 import express from 'express';
 import cors from 'cors';
 import postgres from 'postgres';
+import { series } from 'async'
+import { stringify } from 'csv-stringify/sync'
 
 (function start(){
 
@@ -12,40 +14,66 @@ import postgres from 'postgres';
 	let app = express();
 	app.use(cors());
 	app.use(express.static('www'))
+	ws(app);
 
-	app.post("/search", async (req,res)=>{
+	app.ws("/search", function(ws, req) {
+		let status = {
+			done: 0,
+			total: LAYOUT["1"]+LAYOUT["2"]+1,
+			hits: 0
+		}
 		let results = [];
+		let queue = [];
 
-		for (let spa in LAYOUT) {
-			for (let spb = 1; spb <= LAYOUT[spa]; spb++) {
-				try {
-					let _where_ = false;
-					let where = function(){
-						let value = _where_ ? "AND" : "WHERE";
-						_where_ = true;
-						return value;
-					}
-					const { first, last, city, state, zip, ssn } = req.params;
-					const table = `npd_${spa}_${spb < 10 ? "0"+spb : spb}`;
-					const hits = await sql`select id, first, middle, last, suffix, address, city, state, zip, phone, dob, altdob1, ssn
-					 from ${ sql(table) }${
-						first ? sql` ${where()} first = ${ first }` : sql``}${
-						last ? sql` ${where()} last = ${ last }` : sql``}${
-						city ? sql` ${where()} city = ${ city }` : sql``}${
-						state ? sql` ${where()} state = ${ state }` : sql``}${
-						zip ? sql` ${where()} zip = ${ zip }` : sql``}${
-						ssn ? sql` ${where()} ssn = ${ ssn }` : sql``}`
-					console.log ("searched ", table, hits.length, "results.");
-					if (hits && hits.length > 0) {
-						results = [...results, ...hits];
-					}
-				} catch (ex) {
-					console.log(ex);
+		ws.on("message", m=>{
+			let data = JSON.parse(m);
+			for (let spa in LAYOUT) {
+				for (let spb = 1; spb <= LAYOUT[spa]; spb++) {
+					(function search(table, { first, last, city, state, zip, ssn }){
+						queue.push(function(cb){
+							try {
+								let _where_ = false;
+								let where = function(){
+									let value = _where_ ? "AND" : "WHERE";
+									_where_ = true;
+									return value;
+								}
+
+								const hits = await sql`select id, first, middle, last, suffix, address, city, state, zip, phone, dob, altdob1, ssn
+								 	from ${ sql(table) }${
+									first ? sql` ${where()} first = ${ first }` : sql``}${
+									last ? sql` ${where()} last = ${ last }` : sql``}${
+									city ? sql` ${where()} city = ${ city }` : sql``}${
+									state ? sql` ${where()} state = ${ state }` : sql``}${
+									zip ? sql` ${where()} zip = ${ zip }` : sql``}${
+									ssn ? sql` ${where()} ssn = ${ ssn }` : sql``}`
+								
+								if (hits && hits.length > 0) {
+									status.hits += hits.length;
+									ws.send(JSON.stringify({
+										status,
+										hits,
+										final: false
+									}))
+									results = [...results, ...hits];
+									setTimeout(cb,0)
+								}
+							} catch (ex) {
+								console.log(ex);
+								cb();
+							}
+						})
+					})(`npd_${spa}_${spb < 10 ? "0"+spb : spb}`, data);
 				}
 			}
-		}
+			series(queue, function(){
+				ws.send(JSON.stringify({
+					csv: btoa(stringify(results, { header: true })),
+					final: true;
+				}))
+			})
+		})
 
-		res.json(results);
 	})
 
 	app.listen(8989);
--- webapp/www/index.js
+++ webapp/www/index.js
@@ -1,34 +1,66 @@
 $(document).ready(function(){
   const template = $("#table-template").detach().html();
+  const receipt = $("#head-template").detach().html();
+  let snum = 0;
 
   $("#search").on("submit", event => {
     event.preventDefault();
     const data = $("#search").serialize();
+    snum++;
+
+    let results = [];
 
     (async()=>{
-      let results = await fetch("/search", { method: "post", body: new URLSearchParams(data) });
-      if (results.length > 0) {
-        $("#welcome").hide();
-        let table = $(template);
-        let tbody = $(table).find("tbody");
-        for (let i of results) {
-          tbody.attach($(`<tr>
-            <td>${i.first}</td>
-            <td>${i.middle}</td>
-            <td>${i.last}</td>
-            <td>${i.suffix}</td>
-            <td>${i.address}</td>
-            <td>${i.city}</td>
-            <td>${i.state}</td>
-            <td>${i.zip}</td>
-            <td>${i.phone}</td>
-            <td>${i.dob}</td>
-            <td>${i.altdob1}</td>
-            <td>${i.ssn}</td>
-          </tr>`));
-        }
-        $("#results").attach(table);
+      const socket = new WebSocket("/search");
+      let $body = $(template);
+      $("#results").attach($body);
+      $("#welcome").detach();
+      $body.find(".snum").text(snum);
+      $("#toggly").prop("disabled", true);
+
+      const controls = {
+        progress: $body.find(".progress"),
+        status: $body.find(".status"),
+        bar: $body.find(".progress-bar"),
+        table = $body.find("tbody")
       }
+
+      socket.addEventListener("message", function(event){
+        let message = JSON.parse(event.data);
+        if (!message.final) {
+          if (message.hits) {
+            results = [ ...results, ...message.hits ];
+
+            for (let i of message.hits) {
+              controls.table.attach($(`<tr>
+                <td>${i.first}</td>
+                <td>${i.middle}</td>
+                <td>${i.last}</td>
+                <td>${i.suffix}</td>
+                <td>${i.address}</td>
+                <td>${i.city}</td>
+                <td>${i.state}</td>
+                <td>${i.zip}</td>
+                <td>${i.phone}</td>
+                <td>${i.dob}</td>
+                <td>${i.altdob1}</td>
+                <td>${i.ssn}</td>
+              </tr>`));
+            }
+          }
+          let percent = Math.round((message.status.done / message.status.total) * 100);
+          controls.progress.attr("aria-valuenow", percent);
+          controls.bar.css({ width: `${percent}%` });
+          if (percent >= 10) {
+            controls.bar.text(`${percent} %`);
+          }
+        } else {
+          controls.status.html(`<a href="data:application/json;base64,${btoa(JSON.stringify(results, null, 2))}" download="npd-${snum}.json">JSON</a> | 
+            <a href="data:text/csv;base64,${message.csv}" download="npd-${snum}.csv">CSV</a>`);
+
+          $("#toggly").prop("disabled", false);
+        }
+      })
     })()
 
     return false;
